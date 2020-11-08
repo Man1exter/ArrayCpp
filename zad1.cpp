e74bc65c689dca40bc8f37c677c45ac0a32cd348
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-08 12:58:50 +0100

    commit changes with random array

--- zad1.cpp
+++ zad1.cpp
@@ -13,13 +13,19 @@ using namespace std;
 int main(){
 
 int array[10]{};
+int j;
 
 srand( time( NULL ) ); // ziarno do randomowych liczb..
 
+for( int i = 0; i < 10; i++ )
+    {
+        for ( int j = 1; j < 10; j++){
+        
+    array[i] = (( rand() % 100 ) + 1 );
 
-
-
-cout << array[10] << endl;
+        cout << j << " Wylosowana liczba: " << array[i] << endl;
+    }
+}
 
 return 0;
 }

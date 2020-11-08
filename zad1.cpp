728a4233750b413508c739eafba29bbcfdec8b46
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-08 12:31:48 +0100

    add new library to get random numbers to array

--- zad1.cpp
+++ zad1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime> //biblioteka do inicjalizacji randomowych liczb..
 
 
 
@@ -6,10 +7,17 @@ using namespace std;
 
 
 
+
+
+
 int main(){
 
+srand( time( NULL ) ); // ziarno do randomowych liczb..
 
+int arr[10][10] = {
+    
 
+};
 
 
     return 0;

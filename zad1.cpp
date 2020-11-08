00ec738a32eb1a47e7c1e6447ffea36cd3a5afe7
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-08 14:37:51 +0100

    changes with function to complete max value

--- zad1.cpp
+++ zad1.cpp
@@ -7,6 +7,21 @@ using namespace std;
 
 
 
+int maksymalnaWartosc(int array[10], int a)
+{
+int i;
+int max;
+
+max = array[0];     
+for( i = 0; i < a ; i++) 
+if(array[i] > max) 
+max = array[i];
+ 
+return max;
+}
+
+
+
 
 int main(){
 
@@ -20,7 +35,10 @@ for( int i = 0; i < 10; i++ )
     array[i] = (( rand() % 100 ) + 1 ); // losowanie randomowych liczb..
 
         cout << "Wylosowana liczba: " << array[i] << endl;
+
 }
+
+maksymalnaWartosc(array[10]);
 
    return 0;
 }

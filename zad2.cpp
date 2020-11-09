4bbedfcc0f3caffc578df1342558dcc77beaa884
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-09 19:03:26 +0100

    add comment to exercises with punkt/s how to do

--- zad2.cpp
+++ zad2.cpp
@@ -15,6 +15,13 @@ int main(){
     return 0;
 }
 
+// 1. element w tablicy zajmuje miejsce sasiada w prawo,
+// 2. ostatni element zajmuje miejsce zerowe (przesuniecie),
+// 3. trzy instrukcje niżej .. . .. . . . .. . . . .  buf,
+// 4. buf ostatni przejsciowy eleement tablicy,
+
+
+
 // cyklicznosc podpowiedz o K w prawo
 
 //int buf=tab[r-1];

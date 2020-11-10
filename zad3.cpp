f69137a361f5816e45657becf98de52293144ba2
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-10 19:29:04 +0100

    comment array conditions to done the task

--- zad3.cpp
+++ zad3.cpp
@@ -8,6 +8,17 @@ using namespace std;
 
 int main(){
 
+int array[5]{};
+int numbers;
+
+cout << "Podaj numery swojej tablicy do 5 liczb: " << numbers << endl;
+cin >> numbers;
+
+cout << "Twoje numery w tablicy to: " << array[5] << endl;
+
+
+
+
 
 
 

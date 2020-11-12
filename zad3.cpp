db24673a4bd64b137cda86917339fb2112d53589
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-12 09:43:06 +0100

    get issues to code with array and belock

--- zad3.cpp
+++ zad3.cpp
@@ -6,13 +6,33 @@ using namespace std;
 
 
 
+void przeciwne(int* arr, int liczby, int przeciwna){
+
+przeciwna = arr[i];
+
+cout << "Przeciwne liczby to: " << przeciwna << endl;
+}
+
+
+
+
+
 
 
 int main(){
 
+             int liczby;
 
+cout << "Podaj ile liczb ma miec tablica: " << endl;
+cin >> liczby;
 
+cout << "Podaj liczby do tablicy: " << endl;
 
+int* arr = new int[liczby];
+
+for(int i = 0; i < liczby; i++)
+
+cin >> arr[i];
 
     return 0;
 }

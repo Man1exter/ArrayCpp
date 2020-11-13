85f2ed45b930dc0c49a08c79c0a79c4f4cbe4098
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-13 11:48:17 +0100

    changes save the pokaz-tab in result to sum cpp

--- zad4.cpp
+++ zad4.cpp
@@ -1,22 +1,33 @@
-
 #include <conio.h>
 #include <stdio.h>
 #include <math.h>
+#include <iostream>
+
+
+using namespace std;
+
 
 
 
 const int LW = 20, LK = 30;
 
-char tab[LW][LK];         // tablica globalna
+char tab[LW][LK];         // tablica globalna tab[x][y]
+
+
+
 
 
 void pokaz_tab(){
 
+int nierownosc1, nierownosc2;
 
 
-(x-13)​2​ + (y-10)​2​ <= 64;
 
-y < 1.15 * x;
+cout << endl;
+
+
+cout << "Pierwsza nierównosc: " << nierownosc1 << endl;
+cout << "Druga nierównosc: " << nierownosc2 << endl;
 
 
 }
@@ -27,18 +38,24 @@ void rozwiazanie_tab(){
 
     int nierownosc1, nierownosc2;
 
+    int *wsk1;
+    int *wsk2;
+
+
         for (int y = 0; y < LW; y++)
 
         for (int x = 0; x < LK; x++){
 
-            nierownosc1 = (pow(x-13.0,2.0)+pow(y-10.0,2.0) <= 64);
-            nierownosc2 = (y<1.15*x);
+            nierownosc1 = (pow(x - 13.0,2.0) + pow(y - 10.0,2.0) <= 64);
+            nierownosc2 = (y < 1.15 * x);
+
+            wsk1 = &nierownosc1;
+            wsk2 = &nierownosc2;
 
 
             if(nierownosc1 && nierownosc2) tab[y][x] = '#';
 
-            else tab[y][x]='+';
-
+            else tab[y][x] = '+';
 
             }
 }
@@ -50,7 +67,7 @@ void rozwiazanie_tab(){
 
 int main(){
     
-
+int nierownosc1, nierownosc2;
 
     rozwiazanie_tab();
 

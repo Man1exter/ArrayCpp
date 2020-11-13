631ac2a265a043a55fe904f60c33e787aa7afcec
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-13 12:46:39 +0100

    first commit in new task to do 5 list

--- zad5.cpp
+++ zad5.cpp
@@ -6,11 +6,25 @@ using namespace std;
 
 
 
+void iloscSzukana(){
+
+
+}
+
+
+
 int main(){
 
+int wartosc;
+int ilosc;
 
+cout << "Podaj liczby, ktore zostana zliczone: " << endl;
+cin >> wartosc;
 
-
+iloscSzukana();
 
     return 0;
 }
+
+// Napisz program zawierający funkcję zliczającą ilość wystąpień zadanego znaku,
+// w podanym przez użytkownika za pomocą klawiatury łańcuchu znaków.

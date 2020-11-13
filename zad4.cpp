ab7af478f1f91d830480265b9103477fbfc90242
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-13 10:11:02 +0100

    open result with new exercises to unlock num x/y

--- zad4.cpp
+++ zad4.cpp
@@ -1,16 +1,78 @@
-#include <iostream>
+
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
 
 
 
-using namespace std;
+const int LW = 20, LK = 30;
+char tab[LW][LK]; 
+
+// tablica globalna
+
+void pokaz_tab();
+
+//uklad nierownosci - rozwiazanie
+
+(x-13)​2​ + (y-10)​2​ <= 64;
+
+y < 1.15 * x;
+
+void rozwiazanie_tab();
+
+
+
 
 
 
 int main(){
+    
 
 
+
+    rozwiazanie_tab();
+    
+
+
+
+
+    pokaz_tab();
+    
+
+
+
+
+    getch();
+    
 
 
 
     return 0;
-}
+    }
+    
+    void rozwiazanie_tab(){
+
+
+        int nierownosc1, nierownosc2;
+
+        for (int y = 0; y < LW; y++)
+
+        for (int x = 0; x < LK; x++){
+
+            nierownosc1=(pow(x-13.0,2.0)+pow(y-10.0,2.0) <= 64);
+            nierownosc2=(y<1.15*x);
+
+
+            if(nierownosc1 && nierownosc2) tab[y][x] = '#';
+
+            else tab[y][x]='+';
+
+
+            }
+            }
+
+
+
+// Funkcja    ​rozwiazanie_tab() ​   sprawdza za pomocą instrukcji warunkowej, czy dla danej pary ​x,y spełnione są obie nierówności jednocześnie.
+
+// Rozszerz program o funkcję ​pokaz_tab()​, która wyświetla na ekranie komputera rozwiązanieukładu nierówności. ​Wyświetlanie elementu tablicy wykonaj za pomocą instrukcji ​printf("%2c",tab[y][x]);

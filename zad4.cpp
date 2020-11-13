6810235e7b7562329c4dd9dbb1e15bacbc87332e
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-13 10:42:22 +0100

    delete useless comment on down to code static

--- zad4.cpp
+++ zad4.cpp
@@ -6,9 +6,9 @@
 
 
 const int LW = 20, LK = 30;
-char tab[LW][LK]; 
 
-// tablica globalna
+char tab[LW][LK];         // tablica globalna
+
 
 void pokaz_tab(){
 
@@ -20,7 +20,28 @@ y < 1.15 * x;
 
 
 }
-void rozwiazanie_tab();
+
+
+
+void rozwiazanie_tab(){
+
+    int nierownosc1, nierownosc2;
+
+        for (int y = 0; y < LW; y++)
+
+        for (int x = 0; x < LK; x++){
+
+            nierownosc1 = (pow(x-13.0,2.0)+pow(y-10.0,2.0) <= 64);
+            nierownosc2 = (y<1.15*x);
+
+
+            if(nierownosc1 && nierownosc2) tab[y][x] = '#';
+
+            else tab[y][x]='+';
+
+
+            }
+}
 
 
 
@@ -31,8 +52,8 @@ int main(){
     
 
 
-
     rozwiazanie_tab();
+
     pokaz_tab();
     
 
@@ -41,34 +62,8 @@ int main(){
     
 
 
-
     return 0;
     }
+
     
     
-    void rozwiazanie_tab(){
-
-
-        int nierownosc1, nierownosc2;
-
-        for (int y = 0; y < LW; y++)
-
-        for (int x = 0; x < LK; x++){
-
-            nierownosc1=(pow(x-13.0,2.0)+pow(y-10.0,2.0) <= 64);
-            nierownosc2=(y<1.15*x);
-
-
-            if(nierownosc1 && nierownosc2) tab[y][x] = '#';
-
-            else tab[y][x]='+';
-
-
-            }
-            }
-
-
-
-// Funkcja    ​rozwiazanie_tab() ​   sprawdza za pomocą instrukcji warunkowej, czy dla danej pary ​x,y spełnione są obie nierówności jednocześnie.
-
-// Rozszerz program o funkcję ​pokaz_tab()​, która wyświetla na ekranie komputera rozwiązanieukładu nierówności. ​Wyświetlanie elementu tablicy wykonaj za pomocą instrukcji ​printf("%2c",tab[y][x]);

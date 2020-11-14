7221550d2a912f9daaa27ee837c8f39f379b893e
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-14 17:03:50 +0100

    add wskaznik save user numbers and show howm many

--- zad5.cpp
+++ zad5.cpp
@@ -15,16 +15,23 @@ void iloscSzukana(){
 
 int main(){
 
+int *wskaznik;
 int wartosc;
 int ilosc;
 
 cout << "Podaj liczby, ktore zostana zliczone: " << endl;
 cin >> wartosc;
 
+
+
 iloscSzukana();
 
     return 0;
 }
+
+
+
+
 
 // Napisz program zawierający funkcję zliczającą ilość wystąpień zadanego znaku,
 // w podanym przez użytkownika za pomocą klawiatury łańcuchu znaków.

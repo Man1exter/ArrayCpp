8ed9dde2188840fd6c7f457283a7a56b49f94666
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-14 18:12:37 +0100

    add two dot to end of complain text endpoint

--- zad5.cpp
+++ zad5.cpp
@@ -13,13 +13,13 @@ using namespace std;
 int main(){
 
 
-char zdanie[200];             // ograniczenie do 200 znakow
+char wyraz[200];             // ograniczenie do 200 znakow
 int liczenie[123]{};       // duze i male 122 najwieksze + 1 = 123..
 
 int i = 0; // index w tablicy;
 
 
-cout << "Podaj zdanie ktore zostanie zliczone na znaki: " << endl;
+cout << "Podaj wyraz ktory zostanie zliczony na znaki: " << endl;
 cin >> zdanie;
 
 
@@ -37,7 +37,7 @@ for(int i = 97; i < 123; i++){
                                              // male liter 97 - 122 ------- duze liter 65 - 90 // +1 jako miejsce zerowe'
     if( liczenie[i] ){
 
-        cout << "znak: " << (char)i << " wystepuje: " << liczenie[i] << " razy" << endl;
+        cout << "znak: " << (char)i << " wystepuje: " << liczenie[i] << " razy.." << endl;
     }
 }
 
@@ -47,7 +47,7 @@ for(int i = 65; i < 91; i++){
                                              // male liter 97 - 122 ------- duze liter 65 - 90 // +1 jako miejsce zerowe'
     if( liczenie[i] ){
         
-        cout << "znak: " << (char)i << " wystepuje: " << liczenie[i] << " razy" << endl;
+        cout << "znak: " << (char)i << " wystepuje: " << liczenie[i] << " razy.." << endl;
     }
 }
 

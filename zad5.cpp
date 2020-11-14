7022e2d0db6de286cd16a511f72d5d9722b3fd71
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-14 18:08:05 +0100

    complete all list with arrays to search char  text

--- zad5.cpp
+++ zad5.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <cstdlib>
 
 
 
@@ -13,48 +13,47 @@ using namespace std;
 int main(){
 
 
-char zdanie[101];
-int liczenie[123]{};
+char zdanie[200];             // ograniczenie do 200 znakow
+int liczenie[123] = {};       // duze i male 122 najwieksze + 1 = 123..
 
-int index = 0; // index w tablicy;
+int i = 0; // index w tablicy;
 
 
 cout << "Podaj zdanie ktore zostanie zliczone na znaki: " << endl;
 cin >> zdanie;
 
 
-while(zdanie[index])
+while(zdanie[i])
 {
 
-liczenie[zdanie[index]]++;
+liczenie[zdanie[i]]++;
 
-index++;
+i++;
 
 }
 
 
 for(int i = 97; i < 123; i++){        
                                              // male liter 97 - 122 ------- duze liter 65 - 90 // +1 jako miejsce zerowe'
-    if(liczenie[index] < 0){
+    if( liczenie[i] ){
 
-        cout << "Posiadane zdanie ma: " << (char)index << " - " << liczenie[index] << endl;
+        cout << "znak: " << (char)i << " wystepuje: " << liczenie[i] << " razy" << endl;
     }
 }
 
-for(int i = 65; i < 91; i++){        
-                                             // male liter 97 - 122 ------- duze liter 65 - 90 // +1 jako miejsce zerowe'
-    if(liczenie[index] < 0){
         
-        cout << "Posiadane zdanie ma: " << (char)index << " - " << liczenie[index] << endl;
+
+for(int i = 65; i < 91; i++){      
+                                             // male liter 97 - 122 ------- duze liter 65 - 90 // +1 jako miejsce zerowe'
+    if( liczenie[i] ){
+        
+        cout << "znak: " << (char)i << " wystepuje: " << liczenie[i] << " razy" << endl;
     }
 }
 
-    return 0;
+        
+        return 0;
+
 }
+    
 
-
-
-
-
-// Napisz program zawierający funkcję zliczającą ilość wystąpień zadanego znaku,
-// w podanym przez użytkownika za pomocą klawiatury łańcuchu znaków.

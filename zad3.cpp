41625e637edf12ccd1f620943c5d6ce6a0bedf65
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-06 18:09:06 +0100

    add drawio file with block to ex.2/ex.3

--- zad3.cpp
+++ zad3.cpp
@@ -1,25 +1,16 @@
 #include<iostream>
 #include<cstdlib>
 
-
-
 using namespace std;
 
-
- 
 void przeciwny(int tab[])
 {
         for(int i = 0; i < 10; i++)
                 tab[i] *= (-1); 
 }
  
-
-
-
-
 int main()
 {
-        
         int array[10] = {1, 5, 8, 22, 61, 17, 11, 15, 10, 87}; 
         
         cout << endl;
@@ -30,16 +21,13 @@ int main()
         cout << endl; 
         cout << endl; 
         
-        
         przeciwny(array); 
-        
         
         for(int i = 0; i < 10; i++)
                 cout<< array[i] <<" ";
         
         cout << endl;
         cout << endl;
-        
         
         return 0;
 }

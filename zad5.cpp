85919c4d804730b339a7b7ea907e292ab5c2ad80
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-07 14:26:54 +0100

    add new drawio to file with exercises

--- zad5.cpp
+++ zad5.cpp
@@ -3,29 +3,22 @@
 
 using namespace std;
 
-
 int main(){
-
 
 char wyraz[200];             // ograniczenie do 200 znakow
 int liczenie[123]{};       // duze i male 122 najwieksze + 1 = 123..
 
 int i = 0; // index w tablicy;
 
-
 cout << "Podaj wyraz ktory zostanie zliczony na znaki: " << endl;
 cin >> zdanie;
-
 
 while(zdanie[i])
 {
 
 liczenie[zdanie[i]]++;           // zwiekszenie i dopiero potem przypisanie
-
 i++;
-
 }
-
 
 for(int i = 97; i < 123; i++){        
                                              // male liter 97 - 122 ------- duze liter 65 - 90 // +1 jako miejsce zerowe'
@@ -35,7 +28,6 @@ for(int i = 97; i < 123; i++){
     }
 }
 
-
 for(int i = 65; i < 91; i++){      
                                              // male liter 97 - 122 ------- duze liter 65 - 90 // +1 jako miejsce zerowe'
     if( liczenie[i] ){
@@ -43,9 +35,7 @@ for(int i = 65; i < 91; i++){
         cout << "znak: " << (char)i << " wystepuje: " << liczenie[i] << " razy.." << endl;
     }
 }
-
         return 0;
-
 }
     
 
